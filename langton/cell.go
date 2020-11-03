9061cb992a277b2e3168b88867791f0ac41f3a2e
Metalblueberry <metalblueberry@gmail.com>
2020-11-03 21:15:44 +0100

    document cell

--- langton/cell.go
+++ langton/cell.go
@@ -2,19 +2,23 @@ package langton
 
 import "fmt"
 
+// Cell represents a Position where the ant can walk and the Action it takes
 type Cell struct {
 	Point
 	Step Step
 }
 
+// UpdateNextStep given the sequence of Steps, updates to the next one
 func (cell *Cell) UpdateNextStep(steps []Step) {
 	cell.Step = steps[cell.Step.nextIndex]
 }
 
+// UpdatePreviousStep the opposite of UpdateNextStep
 func (cell *Cell) UpdatePreviousStep(steps []Step) {
 	cell.Step = steps[cell.Step.previousIndex]
 }
 
+// String is the string representation of a Cell
 func (cell *Cell) String() string {
 	return fmt.Sprintf(
 		"%s, %s",
